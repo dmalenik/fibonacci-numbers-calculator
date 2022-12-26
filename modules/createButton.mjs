8f025a47a7a8ab4501bcdefe2558d61e9425585e
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-26 10:24:20 +0100

    refactor: change function parameters

--- modules/createButton.mjs
+++ modules/createButton.mjs
@@ -1,5 +1,5 @@
-const createButton = (form, type, children) => {
-  return `<button form=${form} type=${type}>${children}</button>`;
+const createButton = (form, type, id, children) => {
+  return `<button form=${form} type=${type} id=${id}>${children}</button>`;
 };
 
 export default createButton;
