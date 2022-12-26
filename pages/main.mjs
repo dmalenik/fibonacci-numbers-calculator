c1eb6931c8dc6fa72afb7964445fe4f6f31e70f9
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-26 15:39:16 +0100

    feat(form): set event listeners

--- pages/main.mjs
+++ pages/main.mjs
@@ -1,12 +1,46 @@
 import form from "../fragments/form.mjs";
+import calcFibonacciNums from "../modules/calcFibonacciNums.mjs";
 import { setEventListener } from "../modules/event-listeners/event-listeners.mjs";
 
 const container = document.querySelector(".container");
 
+// | PAGE SECTIONS
+
 container.innerHTML = form;
 
-const addFibonacciLimitButtonEventListener = setEventListener(
-  "#fibonacci-limit-button",
-  "click",
-  () => console.log("Hello, world!")
+// | EVENT LISTENERS
+
+const addFormEventListener = setEventListener(
+  "#fibonacci-form",
+  "submit",
+  (e) => handleFibonacciForm(e)
 );
+
+function handleFibonacciForm(event) {
+  event.preventDefault();
+  console.log("The form is submitted!");
+}
+
+const addLimitNumberEventListener = setEventListener(
+  "#limit-number",
+  "change",
+  (e) => handleLimitNumber(e)
+);
+
+function handleLimitNumber(event) {
+  document
+    .querySelector("#limit-number")
+    .setAttribute("value", event.target.value);
+}
+
+const addFibonacciBtnEventListener = setEventListener(
+  "#fibonacci-button",
+  "click",
+  (e) => handleFibonacciBtn(e)
+);
+
+function handleFibonacciBtn(event) {
+  let limit = document.querySelector("#limit-number").getAttribute("value");
+
+  calcFibonacciNums(limit);
+}
