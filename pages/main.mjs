b166911b61f30294746b4666ecca404dd00bb7e1
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-26 11:57:11 +0100

    feat(button): add event listener

--- pages/main.mjs
+++ pages/main.mjs
@@ -1,5 +1,12 @@
 import form from "../modules/components/form.mjs";
+import setEventListener from "../modules/setEventListener.mjs";
 
 const container = document.querySelector(".container");
 
 container.innerHTML = form;
+
+const addFibonacciLimitButtonEventListener = setEventListener(
+  "#fibonacci-limit-button",
+  "click",
+  () => console.log("Hello, world!")
+);
