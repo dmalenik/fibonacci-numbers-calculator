0df499f7752a4cb4ac8db65807c6d62004af2998
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-30 12:21:18 +0100

    refactor: apply renaming for configs

---
+++ configs/fibonacciNumbersFormConfigs.mjs
@@ -0,0 +1,21 @@
+const formConfig = {
+  id: "fibonacci-form",
+  method: "",
+  action: "",
+};
+const inputConfig = {
+  id: "fibonacci-numbers",
+  type: "number",
+  name: "fibonacci-numbers",
+};
+const labelConfig = {
+  name: "Type something",
+};
+const buttonConfig = {
+  id: "fibonacci-button",
+  type: "button",
+  form: "",
+  children: "Send",
+};
+
+export { formConfig, inputConfig, labelConfig, buttonConfig };
---
+++ configs/fibonacciNumbersOutputConfigs.mjs
@@ -0,0 +1,9 @@
+const sectionConfig = {
+  id: "output",
+};
+const headingConfig = {
+  size: 3,
+  text: "Hello, world!",
+};
+
+export { sectionConfig, headingConfig };
