1dd5507b54177c886462162c84b5c71319a83842
Dmitriy Malenik <dmalenik@gmail.com>
2023-01-04 19:12:37 +0100

    feat: add style configurations and set styles for container

--- pages/main.mjs
+++ pages/main.mjs
@@ -6,6 +6,8 @@ import normalize from "../assets/styles/normalize.css" assert { type: "css" };
 import global from "../assets/styles/global.css" assert { type: "css" };
 import utilities from "../assets/styles/utilities.css" assert { type: "css" };
 import container from "../assets/styles/container.css" assert { type: "css" };
+import form from "../assets/styles/form.css" assert { type: "css" };
+import output from "../assets/styles/output.css" assert { type: "css" };
 
 const { handleLimitNumber, handleFibonacciBtn } = eventHandlers;
 
@@ -29,6 +31,15 @@ const addFibonacciBtnEventListener = setEventListener(
 
 // | STYLES
 
-document.adoptedStyleSheets = [normalize, global, utilities, container];
+document.adoptedStyleSheets = [
+  normalize,
+  global,
+  utilities,
+  container,
+  form,
+  output,
+];
 
-document.querySelector(".container").classList.add("container");
+document
+  .querySelector(".container")
+  .classList.add("f-col", "f-x-center", "f-y-center");
