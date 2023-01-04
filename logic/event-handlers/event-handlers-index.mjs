bb6a49fe4ab4eb1279af38a091851ba8cc73236c
Dmitriy Malenik <dmalenik@gmail.com>
2023-01-04 15:23:02 +0100

    refactor: change code syntax, remove code

--- logic/event-handlers/event-handlers-index.mjs
+++ logic/event-handlers/event-handlers-index.mjs
@@ -1,3 +1,2 @@
 export { handleFibonacciBtn } from "./handleFibonacciBtn.mjs";
-export { handleFibonacciForm } from "./handleFibonacciForm.mjs";
 export { handleLimitNumber } from "./handleLimitNumber.mjs";
