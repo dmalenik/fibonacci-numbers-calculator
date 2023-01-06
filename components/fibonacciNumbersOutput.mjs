949e4f29483140954bd3f9b6009ff3af854ca9ec
Dmitriy Malenik <dmalenik@gmail.com>
2023-01-06 16:16:30 +0100

    feat(tryAgainBtn): add button and set a logic to it

--- components/fibonacciNumbersOutput.mjs
+++ components/fibonacciNumbersOutput.mjs
@@ -15,8 +15,23 @@ numbers.setAttribute("id", "numbers");
 
 numbers.classList.add("w-100", "t-center");
 
+const tryAgainBtn = document.createElement("button");
+
+tryAgainBtn.setAttribute("id", "again");
+tryAgainBtn.setAttribute("type", "button");
+
+tryAgainBtn.innerText = "Try again";
+
+tryAgainBtn.addEventListener("click", (event) => {
+  output.classList.replace("f-col", "d-hidden");
+  document
+    .querySelector("#fibonacci-form")
+    .classList.replace("d-hidden", "f-row");
+});
+
 output.appendChild(heading);
 output.appendChild(numbers);
+output.appendChild(tryAgainBtn);
 
 fibonacciNumbersOutput.append(output);
 
