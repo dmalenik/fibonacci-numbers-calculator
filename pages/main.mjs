afc4926af4e34f1bf59bf007a852cef002b07de9
Dmitriy Malenik <dmalenik@gmail.com>
2023-01-09 15:40:32 +0100

    'refactor: remove semicolons'

--- pages/main.mjs
+++ pages/main.mjs
@@ -1,4 +1,4 @@
-import { fibonacciNumbersForm } from '../components/fibonacciNumbersForm.mjs';
+import { fibonacciNumbersForm } from '../components/fibonacciNumbersForm.mjs'
 
 import normalize from '../assets/styles/normalize.css' assert { type: 'css' };
 import global from '../assets/styles/global.css' assert { type: 'css' };
@@ -8,12 +8,12 @@ import form from '../assets/styles/form.css' assert { type: 'css' };
 
 // | PAGE SECTIONS
 
-document.querySelector('.container').appendChild(fibonacciNumbersForm);
+document.querySelector('.container').appendChild(fibonacciNumbersForm)
 
 // | STYLES
 
-document.adoptedStyleSheets = [normalize, global, utilities, container, form];
+document.adoptedStyleSheets = [normalize, global, utilities, container, form]
 
 document
-  .querySelector('.container')
-  .classList.add('f-col', 'f-x-center', 'f-y-center');
+    .querySelector('.container')
+    .classList.add('f-col', 'f-x-center', 'f-y-center')
