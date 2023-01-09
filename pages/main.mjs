ab9bb31fa807fea3a454b826b8ab0d81f46a83cb
Dmitriy Malenik <dmalenik@gmail.com>
2023-01-09 12:39:56 +0100

    'fix(main): change double quotes to single quotes'

--- pages/main.mjs
+++ pages/main.mjs
@@ -1,19 +1,19 @@
-import { fibonacciNumbersForm } from "../components/fibonacciNumbersForm.mjs";
+import { fibonacciNumbersForm } from '../components/fibonacciNumbersForm.mjs';
 
-import normalize from "../assets/styles/normalize.css" assert { type: "css" };
-import global from "../assets/styles/global.css" assert { type: "css" };
-import utilities from "../assets/styles/utilities.css" assert { type: "css" };
-import container from "../assets/styles/container.css" assert { type: "css" };
-import form from "../assets/styles/form.css" assert { type: "css" };
+import normalize from '../assets/styles/normalize.css' assert { type: 'css' };
+import global from '../assets/styles/global.css' assert { type: 'css' };
+import utilities from '../assets/styles/utilities.css' assert { type: 'css' };
+import container from '../assets/styles/container.css' assert { type: 'css' };
+import form from '../assets/styles/form.css' assert { type: 'css' };
 
 // | PAGE SECTIONS
 
-document.querySelector(".container").appendChild(fibonacciNumbersForm);
+document.querySelector('.container').appendChild(fibonacciNumbersForm);
 
 // | STYLES
 
 document.adoptedStyleSheets = [normalize, global, utilities, container, form];
 
 document
-  .querySelector(".container")
-  .classList.add("f-col", "f-x-center", "f-y-center");
+  .querySelector('.container')
+  .classList.add('f-col', 'f-x-center', 'f-y-center');
