25ad94fa8aa4f4fc6f8706139641444453625efd
Dmitriy Malenik <dmalenik@gmail.com>
2023-01-10 10:46:43 +0100

    'refactor: change fragment to functions'

--- pages/main.mjs
+++ pages/main.mjs
@@ -1,14 +1,14 @@
-import { fibonacciNumbersForm } from '../components/fibonacciNumbersForm.mjs'
+import fibonacciNumbersForm from '../components/fibonacciNumbersForm.mjs'
 
-import normalize from '../assets/styles/normalize.css' assert { type: 'css' };
-import global from '../assets/styles/global.css' assert { type: 'css' };
-import utilities from '../assets/styles/utilities.css' assert { type: 'css' };
-import container from '../assets/styles/container.css' assert { type: 'css' };
-import form from '../assets/styles/form.css' assert { type: 'css' };
+import normalize from '../assets/styles/normalize.css' assert { type: 'css' }
+import global from '../assets/styles/global.css' assert { type: 'css' }
+import utilities from '../assets/styles/utilities.css' assert { type: 'css' }
+import container from '../assets/styles/container.css' assert { type: 'css' }
+import form from '../assets/styles/form.css' assert { type: 'css' }
 
 // | PAGE SECTIONS
 
-document.querySelector('.container').appendChild(fibonacciNumbersForm)
+document.querySelector('.container').appendChild(fibonacciNumbersForm())
 
 // | STYLES
 
