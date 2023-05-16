91f1d7a9337fddbe3b6bb72866a85ad2bc001b9c
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-16 15:55:40 +0300

    refactor: remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import fibonacciNumbersForm from './components/fibonacciNumberOutput.js'
 import fibonacciNumberOutput from './components/fibonacciNumbersForm.js'
-import createCSSLink from './logic/createCSSLink.js'
 import './assets/styles/normalize.css'
 import './assets/styles/global.css'
 import './assets/styles/utilities.css'
 import './assets/styles/container.css'
 import './assets/styles/form.css'
 
-document.querySelector('.container').appendChild(fibonacciNumbersForm())
-document.querySelector('.container').appendChild(fibonacciNumberOutput())
-document
-    .querySelector('.container')
-    .classList.add('f-col', 'f-x-center', 'f-y-center')
+document.body.appendChild(fibonacciNumbersForm())
+document.body.appendChild(fibonacciNumberOutput())
+document.body.classList.add('f-col', 'f-x-center', 'f-y-center')
