9bd99de71ba291f9e30ff7dd054c21e28087ea43
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-16 14:05:07 +0300

    'feat: add configurations for output management
    - add html webpack plugin in the configurations
    - add automated cleaning options
    '

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Fibonacci Numbers Generator'
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
