8d50478e4c929bc4675ce04fb69613d5c9ab68a3
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-16 16:27:49 +0300

    feat: implement Hot Module Replacement options

--- src/index.js
+++ src/index.js
@@ -6,6 +6,28 @@ import './assets/styles/utilities.css'
 import './assets/styles/container.css'
 import './assets/styles/form.css'
 
-document.body.appendChild(fibonacciNumbersForm())
-document.body.appendChild(fibonacciNumberOutput())
 document.body.classList.add('f-col', 'f-x-center', 'f-y-center')
+
+let form = fibonacciNumbersForm()
+document.body.appendChild(form)
+
+if (module.hot) {
+  module.hot.accept('./components/fibonacciNumbersForm.js', function() {
+    console.log('Accepting the updated fibonacciNumbersForm')
+    document.body.removeChild(form)
+    form = fibonacciNumbersForm()
+    document.body.appendChild(form)
+  })
+}
+
+let output = fibonacciNumberOutput()
+document.body.appendChild(output)
+
+if (module.hot) {
+  module.hot.accept('./components/fibonacciNumberOutput.js', function() {
+    console.log('Accepting the updated fibonacciNumberOutput')
+    document.body.removeChild(output)
+    output = fibonacciNumberOutput()
+    document.body.appendChild(output)
+  })
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
+    hot: true,
   },
   optimization: {
     runtimeChunk: 'single',
