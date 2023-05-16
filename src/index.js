b1ae31f69cecb8976ac03f03d5b3a2de1259339a
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-16 13:20:53 +0300

    refactor: remove css link creator function

--- src/index.js
+++ src/index.js
@@ -7,23 +7,8 @@ import './assets/styles/utilities.css'
 import './assets/styles/container.css'
 import './assets/styles/form.css'
 
-// | PAGE SECTIONS
-
 document.querySelector('.container').appendChild(fibonacciNumbersForm())
 document.querySelector('.container').appendChild(fibonacciNumberOutput())
-
-// | STYLES
-
-const styles = [
-    './assets/styles/normalize.css',
-    './assets/styles/global.css',
-    './assets/styles/utilities.css',
-    './assets/styles/container.css',
-    './assets/styles/form.css',
-]
-
-styles.forEach((style) => createCSSLink(style))
-
 document
     .querySelector('.container')
     .classList.add('f-col', 'f-x-center', 'f-y-center')
