ad844dfbd3d4f142b392acdfb21b8d252ef06899
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-21 19:57:39 +0300

    refactor: apply linter rules
    - set variable names to camelCase syntax
    - change Math.pow to **-sign operator

--- src/features/input/services/calcFibonacciNum.js
+++ src/features/input/services/calcFibonacciNum.js
@@ -1,10 +1,10 @@
 const calcFibonacciNum = (queueNumber) => {
     // Binet formula
     const sqrt5 = Math.sqrt(5)
-    const phi_1 = (1 + sqrt5) / 2
-    const phi_2 = (1 - sqrt5) / 2
-    let fibonacciNumber = Math.round(
-        (Math.pow(phi_1, queueNumber) - Math.pow(phi_2, queueNumber)) / sqrt5
+    const PHI1 = (1 + sqrt5) / 2
+    const PHI2 = (1 - sqrt5) / 2
+    const fibonacciNumber = Math.round(
+        PHI1 ** queueNumber - PHI2 ** queueNumber / sqrt5
     )
 
     return fibonacciNumber
