efa9fffecaca989c1f7bef3bfe6f6b9abd62ca7e
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-23 00:17:42 +0300

    refactor: replace Home page with components

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-import Home from './pages/Home/Home'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 import './app.css'
 
 const App = () => {
@@ -6,15 +8,39 @@ const App = () => {
 
     container.classList.add('app')
 
-    let home = Home()
-    container.appendChild(home)
+    let header = Header()
+    container.appendChild(header)
 
     if (module.hot) {
-        module.hot.accept('./pages/Home/Home.js', () => {
+        module.hot.accept('./components/Header.js', () => {
             console.log('Accepting the updated Home page')
-            container.removeChild(home)
-            home = Home()
-            container.appendChild(home)
+            container.removeChild(header)
+            header = Header()
+            container.appendChild(header)
+        })
+    }
+
+    let main = Main()
+    container.appendChild(main)
+
+    if (module.hot) {
+        module.hot.accept('./components/Main.js', () => {
+            console.log('Accepting the updated Home page')
+            container.removeChild(main)
+            main = Main()
+            container.appendChild(main)
+        })
+    }
+
+    let footer = Footer()
+    container.appendChild(footer)
+
+    if (module.hot) {
+        module.hot.accept('./components/Footer.js', () => {
+            console.log('Accepting the updated Home page')
+            container.removeChild(footer)
+            footer = Footer()
+            container.appendChild(footer)
         })
     }
 
