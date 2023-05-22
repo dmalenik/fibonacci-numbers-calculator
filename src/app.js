c41784be5d238a823473dc4b81eedabcdf44fca5
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-22 23:49:34 +0300

    refactor: replace components with the Home page

--- src/app.js
+++ src/app.js
@@ -1,46 +1,20 @@
-import Header from './components/layout/Header/Header'
-import Main from './components/layout/Main/Main'
-import Footer from './components/layout/Footer/Footer'
+import Home from './pages/Home/Home'
 import './app.css'
 
 const App = () => {
     const container = document.createElement('div')
 
-    container.classList.add('container')
+    container.classList.add('app')
 
-    let header = Header()
-    container.appendChild(header)
-
-    if (module.hot) {
-        module.hot.accept('./components/layout/Header/Header.js', () => {
-            console.log('Accepting the updated Header component')
-            container.removeChild(header)
-            header = Header()
-            container.appendChild(header)
-        })
-    }
-
-    let main = Main()
-    container.appendChild(main)
+    let home = Home()
+    container.appendChild(home)
 
     if (module.hot) {
-        module.hot.accept('./components/layout/Main/Main.js', () => {
-            console.log('Accepting the updated Main container')
-            container.removeChild(main)
-            main = Main()
-            container.appendChild(main)
-        })
-    }
-
-    let footer = Footer()
-    container.appendChild(footer)
-
-    if (module.hot) {
-        module.hot.accept('./components/layout/Footer/Footer.js', () => {
-            console.log('Accepting the updated Footer container')
-            container.removeChild(footer)
-            footer = Footer()
-            container.appendChild(footer)
+        module.hot.accept('./pages/Home/Home.js', () => {
+            console.log('Accepting the updated Home page')
+            container.removeChild(home)
+            home = Home()
+            container.appendChild(home)
         })
     }
 
