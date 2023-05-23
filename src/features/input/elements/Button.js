807825736f086fc2a682d128e96703c2b51c9e1c
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-23 17:25:54 +0300

    feat: create Button element

---
+++ src/features/input/elements/Button.js
@@ -0,0 +1,13 @@
+const Button = () => {
+  const element = document.createElement('button')
+
+    element.setAttribute('id', 'fibonacci-button')
+    element.setAttribute('type', 'button')
+    element.setAttribute('form', 'fibonacci-form')
+
+    element.innerText = 'Send'
+
+    return element
+}
+
+export default Button
