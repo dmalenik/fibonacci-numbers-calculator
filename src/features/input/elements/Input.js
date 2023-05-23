26aa644a4219b71966f6bc45d685ebedb0b1cf4f
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-23 17:17:31 +0300

    feat: create Input element

---
+++ src/features/input/elements/Input.js
@@ -0,0 +1,12 @@
+const Input = () => {
+  const element = document.createElement('input')
+
+  element.setAttribute('id', 'input-number')
+  element.setAttribute('type', 'number')
+  element.setAttribute('name', 'input-number')
+  element.setAttribute('min', 0)
+
+  return element
+}
+
+export default Input
