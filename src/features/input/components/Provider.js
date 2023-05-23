4e919a46e1100b477f6ade0d8b03f79cdc827715
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-23 20:43:51 +0300

    refactor: place provider styles in styles folder

--- src/features/input/components/Provider.js
+++ src/features/input/components/Provider.js
@@ -4,7 +4,7 @@ import Input from '../elements/Input'
 import Button from '../elements/Button'
 import setInputValue from '../services/setInputValue'
 import provideFibonacciNum from '../services/provideFibonacciNum'
-import './provider.css'
+import '../assets/styles/provider.css'
 
 const Provider = () => {
     const section = Form()
