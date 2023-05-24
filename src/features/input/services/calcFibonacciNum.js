09fd80cf6ecb06bdd2f288f74094f6cb95748368
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-24 21:13:40 +0300

    fix: add brackets in the formula function

--- src/features/input/services/calcFibonacciNum.js
+++ src/features/input/services/calcFibonacciNum.js
@@ -4,7 +4,7 @@ const calcFibonacciNum = (queueNumber) => {
     const PHI1 = (1 + sqrt5) / 2
     const PHI2 = (1 - sqrt5) / 2
     const fibonacciNumber = Math.round(
-        PHI1 ** queueNumber - PHI2 ** queueNumber / sqrt5
+        (PHI1 ** queueNumber - PHI2 ** queueNumber) / sqrt5
     )
 
     return fibonacciNumber
