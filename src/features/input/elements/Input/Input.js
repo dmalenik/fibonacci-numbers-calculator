e8998dc73551445b06c01ca44caeadab9143f446
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-24 23:17:05 +0300

    refactor: format files

--- src/features/input/elements/Input/Input.js
+++ src/features/input/elements/Input/Input.js
@@ -1,5 +1,5 @@
 import data from './input.json'
-import createCustomizedElement from '../../utils/createCustomizedElement' 
+import createCustomizedElement from '../../utils/createCustomizedElement'
 
 const Input = createCustomizedElement(data.name, data.attrs, data.children)
 
--- src/features/input/utils/createCustomizedElement.js
+++ src/features/input/utils/createCustomizedElement.js
@@ -1,12 +1,12 @@
 const createCustomizedElement = (name, attrs, children) => {
-  const element = document.createElement(name)
+    const element = document.createElement(name)
 
-  Object.entries(attrs).forEach((attr) => {
-    element.setAttribute(attr[0], attr[1])
-  })
-  element.innerText = children
+    Object.entries(attrs).forEach((attr) => {
+        element.setAttribute(attr[0], attr[1])
+    })
+    element.innerText = children
 
-  return element
+    return element
 }
 
 export default createCustomizedElement
