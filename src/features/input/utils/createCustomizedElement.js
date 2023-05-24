bc6403c91774d9b4eab4544d2bf2eb8f7e9e90b2
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-24 22:13:54 +0300

    feat: create service for elements creation

---
+++ src/features/input/utils/createCustomizedElement.js
@@ -0,0 +1,12 @@
+const createCustomizedElement = (name, attrs, children) => {
+  const element = document.createElement(name)
+
+  Object.entries(attrs).forEach((attr) => {
+    element.setAttribute(attr[0], attr[1])
+  })
+  element.innerText = children
+
+  return element
+}
+
+export default createCustomizedElement
