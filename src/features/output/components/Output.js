add57dec7c6c46b5b9fab2e66b2d31379e21e3e8
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-26 23:45:34 +0300

    refactor: change paths

--- src/features/output/components/Output.js
+++ src/features/output/components/Output.js
@@ -1,6 +1,6 @@
 import Section from '../elements/Section/Section'
 import Button from '../elements/Button/Button'
-import Heading2 from '../elements/Heading-2/Heading-2'
+import Heading2 from '../elements/H2/Heading-2'
 
 const Output = () => {
     Section.appendChild(Heading2)
