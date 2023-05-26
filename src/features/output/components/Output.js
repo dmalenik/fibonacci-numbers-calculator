657206aa0c63ccdd3b754336d6a459994f6f9e9e
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-26 22:32:37 +0300

    refactor: place event listener inside the component module

--- src/features/output/components/Output.js
+++ src/features/output/components/Output.js
@@ -1,11 +1,8 @@
 import Section from '../elements/Section/Section'
 import Button from '../elements/Button/Button'
 import Heading2 from '../elements/Heading-2/Heading-2'
-import returnInput from '../elements/Button/returnInput'
 
 const Output = () => {
-    Button.addEventListener('click', returnInput)
-
     Section.appendChild(Heading2)
     Section.appendChild(Button)
 
--- src/features/output/elements/Button/Button.js
+++ src/features/output/elements/Button/Button.js
@@ -1,6 +1,9 @@
 import data from './Button.data.json'
 import createCustomizedElement from '../../../shared/utils/createCustomizedElement'
+import returnInput from './returnInput'
 
 const Button = createCustomizedElement(data.name, data.attrs, data.children)
+
+Button.addEventListener('click', returnInput)
 
 export default Button
