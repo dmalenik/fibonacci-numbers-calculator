bb3e5aab5896e198360792e2a39943d8fba77e10
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-26 23:46:17 +0300

    refactor: rename files

---
+++ src/features/output/elements/H2/H2.js
@@ -0,0 +1,6 @@
+import data from './H2.data.json'
+import createCustomizedElement from '../../../../utils/createCustomizedElement'
+
+const Heading2 = createCustomizedElement(data.name, data.attrs, data.children)
+
+export default Heading2
