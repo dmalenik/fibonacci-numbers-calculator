45282a39375b51355317a6e0f39ec1236b707c28
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-26 19:19:22 +0300

    refactor: change paths

--- src/features/input/components/Provider.js
+++ src/features/input/components/Provider.js
@@ -1,9 +1,9 @@
 import Form from '../elements/Form/Form'
 import Label from '../elements/Label/Label'
 import Input from '../elements/Input/Input'
+import setInputValue from '../elements/Input/setInputValue'
 import Button from '../elements/Button/Button'
-import setInputValue from '../services/setInputValue'
-import provideFibonacciNum from '../../shared/services/provideFibonacciNum'
+import provideFibonacciNum from '../elements/Button/provideFibonacciNum'
 import './Provider.styles.css'
 
 const Provider = () => {
