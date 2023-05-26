ac019c752efea0cb5fdbf185ee18d4f8b466c3f2
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-27 00:06:36 +0300

    refactor: rename App component styles

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
-import './app.css'
+import './App.styles.css'
 
 const App = () => {
     const container = document.createElement('div')
