780ce0df856813743c960e0106dcb76e86dd43a9
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-26 23:38:57 +0300

    create App components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import HeaderElement from '../elements/HeaderElement/HeaderElement'
-import H1 from '../elements/H1/H1'
+import HeaderElement from './elements/HeaderElement/HeaderElement'
+import H1 from './elements/H1/H1'
 
 const Header = () => {
     HeaderElement.appendChild(H1)
