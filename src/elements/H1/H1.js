c0517869555de5d13a85272f3d6525152b9999bf
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-26 23:08:08 +0300

    feat: create h1 element

---
+++ src/elements/H1/H1.js
@@ -0,0 +1,6 @@
+import createCustomizedElement from '../../utils/createCustomizedElement'
+import data from './H1.data.json'
+
+const H1 = createCustomizedElement(data.name, data.attrs, data.children)
+
+export default H1
