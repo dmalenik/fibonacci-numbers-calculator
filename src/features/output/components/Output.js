8f9663ae5c1b8defab2d940d6b5752e73c997435
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-26 18:50:20 +0300

    refactor: optimize Output components
    - divide component into elements
    - place service in a separate module

--- src/features/output/components/Output.js
+++ src/features/output/components/Output.js
@@ -1,48 +1,15 @@
-import animateElement from '../../shared/services/animateElement'
-import outputAnimations from './Output.animations.json'
-import providerAnimations from '../../input/components/Provider.animations.json'
-import changeClass from '../../../utils/changeClass'
+import Section from '../elements/Section/Section'
+import Button from '../elements/Button/Button'
+import Heading2 from '../elements/Heading-2/Heading-2'
+import returnInput from '../../shared/services/returnInput'
 
 const Output = () => {
-    const section = document.createElement('section')
+    Button.addEventListener('click', returnInput)
 
-    section.setAttribute('id', 'output')
+    Section.appendChild(Heading2)
+    Section.appendChild(Button)
 
-    const heading = document.createElement('h2')
-
-    heading.innerText = 'Display the result of a Fibonacci number calculations'
-
-    const tryAgainBtn = document.createElement('button')
-
-    tryAgainBtn.setAttribute('id', 'again')
-    tryAgainBtn.setAttribute('type', 'button')
-    tryAgainBtn.innerText = 'Try again'
-    tryAgainBtn.addEventListener('click', () => {
-        const outputCenterToLeftAnimation = animateElement(
-            section,
-            outputAnimations.centerToLeft.animations,
-            outputAnimations.centerToLeft.time
-        )
-
-        outputCenterToLeftAnimation.finished.then(() => {
-            changeClass(section, 'f-col', 'd-hidden')
-            changeClass(
-                document.querySelector('#fibonacci-form'),
-                'd-hidden',
-                'f-row'
-            )
-            animateElement(
-                document.querySelector('#fibonacci-form'),
-                providerAnimations.leftToCenter.animations,
-                providerAnimations.leftToCenter.time
-            )
-        })
-    })
-
-    section.appendChild(heading)
-    section.appendChild(tryAgainBtn)
-
-    return section
+    return Section
 }
 
 export default Output
