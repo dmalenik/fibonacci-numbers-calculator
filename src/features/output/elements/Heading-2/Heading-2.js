f711875393ab831d4d838516817dd6729597f2cf
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-26 18:35:58 +0300

    create Heading-2 element

---
+++ src/features/output/elements/Heading-2/Heading-2.js
@@ -0,0 +1,6 @@
+import data from './Heading-2.data.json'
+import createCustomizedElement from '../../../shared/utils/createCustomizedElement'
+
+const Heading2 = createCustomizedElement(data.name, data.attrs, data.children)
+
+export default Heading2
