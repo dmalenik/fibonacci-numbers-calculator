e7c409863cd1c039a850e8ee7c3a28cafa6b1ba4
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-01 00:32:14 +0300

    refactor: apply formatting rules

--- src/utils/createCustomizedElement.js
+++ src/utils/createCustomizedElement.js
@@ -1,6 +1,6 @@
 const createCustomizedElement = ({ name, attrs, children }) => {
     const element = document.createElement(name)
-    
+
     if (Object.entries(attrs).length) {
         Object.entries(attrs).forEach((attr) => {
             element.setAttribute(attr[0], attr[1])
