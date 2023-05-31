405dc79ff1c7eb09ba00db3ff6389e67a03efefb
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-31 23:38:09 +0300

    feat: generate styles file

--- src/elements/Container/Container.js
+++ src/elements/Container/Container.js
@@ -1,6 +1,9 @@
 import data from './Container.data.json'
 import createCustomizedElement from '../../utils/createCustomizedElement'
+import './Container.styles.css'
 
 const Container = createCustomizedElement(data)
+
+Container.classList.add('app')
 
 export default Container
