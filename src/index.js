cb86d4b75cfbe0412668da0c194bf2a51399fdf3
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-01 09:56:11 +0300

    feat: remove utilities styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import App from './App'
 import './assets/styles/normalize.css'
 import './assets/styles/global.css'
-import './assets/styles/utilities.css'
 
 let app = App()
 document.body.appendChild(app)
