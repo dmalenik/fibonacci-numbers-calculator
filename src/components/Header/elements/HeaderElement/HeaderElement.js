ab7b05b870ddb66c8bfc168ae1dbec0681130b0a
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-06 12:44:27 +0300

    feat: set layout styles for Header component

--- src/components/Header/elements/HeaderElement/HeaderElement.js
+++ src/components/Header/elements/HeaderElement/HeaderElement.js
@@ -1,6 +1,9 @@
 import createCustomizedElement from '../../../../utils/createCustomizedElement'
 import data from './headerElement.data.json'
+import './headerElement.styles.scss'
 
 const headerElement = createCustomizedElement(data)
+
+headerElement.classList.add('header')
 
 export default headerElement
