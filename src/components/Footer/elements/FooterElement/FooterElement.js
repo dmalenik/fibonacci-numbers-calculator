4be282fb65227b33b3b6f0fb8e21599babe6e702
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-06 12:45:27 +0300

    feat: set layout styles for Footer component

--- src/components/Footer/elements/FooterElement/FooterElement.js
+++ src/components/Footer/elements/FooterElement/FooterElement.js
@@ -1,6 +1,9 @@
 import createCustomizedElement from '../../../../utils/createCustomizedElement'
 import data from './footerElement.data.json'
+import './footerElement.styles.scss'
 
 const footerElement = createCustomizedElement(data)
+
+footerElement.classList.add('footer')
 
 export default footerElement
