fdd267d925c0a05bd649d9922397c5f48b6e1187
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-06 12:44:56 +0300

    feat: set layout styles for Main component

--- src/components/Main/elements/MainElement/MainElement.js
+++ src/components/Main/elements/MainElement/MainElement.js
@@ -1,6 +1,9 @@
 import createCustomizedElement from '../../../../utils/createCustomizedElement'
 import data from './mainElement.data.json'
+import './mainElement.styles.scss'
 
 const mainElement = createCustomizedElement(data)
+
+mainElement.classList.add('main')
 
 export default mainElement
