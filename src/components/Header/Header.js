3b53ccd2a79676d21908b08a60e752b49eaa4b3e
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-07 16:52:40 +0300

    refactor: applied styles to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import headerElement from './elements/headerElement/headerElement'
 import h1 from './elements/h1/h1'
 
 const Header = () => {
+    headerElement.classList.add('header')
     headerElement.appendChild(h1)
 
     return headerElement
--- src/components/Header/elements/HeaderElement/HeaderElement.js
+++ src/components/Header/elements/HeaderElement/HeaderElement.js
@@ -1,9 +1,6 @@
 import createCustomizedElement from '../../../../utils/createCustomizedElement'
 import data from './headerElement.data.json'
-import './headerElement.styles.scss'
 
 const headerElement = createCustomizedElement(data)
-
-headerElement.classList.add('header')
 
 export default headerElement
