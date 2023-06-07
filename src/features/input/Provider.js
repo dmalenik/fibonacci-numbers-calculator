1aab6b210e6baa863f3d45c8c88a5832eff65686
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-07 16:54:57 +0300

    refactor: remove Provider component from components folder

---
+++ src/features/input/Provider.js
@@ -0,0 +1,14 @@
+import form from './elements/form/form'
+import label from './elements/label/label'
+import input from './elements/input/input'
+import button from './elements/button/button'
+
+const Provider = () => {
+    form.appendChild(label)
+    form.appendChild(input)
+    form.appendChild(button)
+
+    return form
+}
+
+export default Provider
