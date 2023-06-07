081c21dac2d53c7be3d2e1a86ccb9d5b70518437
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-07 16:54:10 +0300

    refactor: apply styles to Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,9 @@
 import footerElement from './elements/footerElement/footerElement'
 import h2 from './elements/h2/h2'
+import './Footer.styles.scss'
 
 const Footer = () => {
+    footerElement.classList.add('footer')
     footerElement.appendChild(h2)
 
     return footerElement
--- src/components/Footer/elements/FooterElement/FooterElement.js
+++ src/components/Footer/elements/FooterElement/FooterElement.js
@@ -1,9 +1,6 @@
 import createCustomizedElement from '../../../../utils/createCustomizedElement'
 import data from './footerElement.data.json'
-import './footerElement.styles.scss'
 
 const footerElement = createCustomizedElement(data)
-
-footerElement.classList.add('footer')
 
 export default footerElement
