fb13517824d5f1b5aa3ea83a4ca33e2ee6a84f6c
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-11 16:02:36 +0300

    feat: implement sessionStorage for Output feature
    - calculate and place Fibonacci number inside sessionStorage
    - take the result from sessionStorage
    - place it as inner Text of the element

--- src/features/output/Output.js
+++ src/features/output/Output.js
@@ -4,7 +4,30 @@ import data from './section.data.json'
 const Output = () => {
     const section = createCustomizedElement(data)
 
+    Promise.all([
+        import('../services/fibonacciNumService'),
+        import('../services/sessionStorageService'),
+    ])
+        .then((modules) => {
+            const calcFibonacciNum = modules[0].default
+            const updateItemToSessionStorage = modules[1].default
+
+            const sequenceNumber = sessionStorage.getItem('sequenceNumber')
+            const fibonacciNum = calcFibonacciNum(sequenceNumber)
+
+            updateItemToSessionStorage('fibonacciNum', fibonacciNum)
+        })
+        .then(() => {
+            import('./elements').then(({ h2, result, button }) => {
+                result.innerText = `The Fibonacci number is ${sessionStorage.getItem(
+                    'fibonacciNum'
+                )}`
+
+                section.appendChild(h2)
                 section.appendChild(result)
+                section.appendChild(button)
+            })
+        })
 
     return section
 }
--- src/features/output/elements/Button/handleOutput.copy.js
+++ src/features/output/elements/Button/handleOutput.copy.js
@@ -1,10 +1,11 @@
-import changeClass from '../../../utils/classUtils'
-
 const handleOutputCopy = () => {
-    // remove Output
-    // import Provider
-    changeClass(document.querySelector('#output'), 'd-flex', 'd-none')
-    changeClass(document.querySelector('#fibonacci-form'), 'd-none', 'd-flex')
+    document
+        .querySelector('.main')
+        .removeChild(document.querySelector('#output'))
+
+    import('../../../input/Provider').then(({ default: Provider }) => {
+        document.querySelector('.main').appendChild(Provider())
+    })
 }
 
 export default handleOutputCopy
