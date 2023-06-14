80a1e0ad2b79f4cf24f20dbc38b707d8b41669fe
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-14 19:10:43 +0300

    feat: implement svg asset

--- src/components/Footer/elements/copyright/copyright.js
+++ src/components/Footer/elements/copyright/copyright.js
@@ -1,10 +1,8 @@
 import data from './copyright.data.json'
 import { createCustomizedElement } from '../../../../utils/DOMUtils'
-import link from '../link/link'
 
 const copyright = createCustomizedElement(data)
 
-copyright.innerText = `${String.fromCharCode(64)} 2023 `
-copyright.appendChild(link)
+copyright.innerText = `${String.fromCharCode(64)} 2023 by Dmitriy Malenik`
 
 export default copyright
