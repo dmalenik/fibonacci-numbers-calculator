1c9d0259565bdcb952350f8494b1bb0a68bc44e0
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-22 11:34:20 +0300

    feat: put notes regarding the calculations inside tests file

--- src/features/test/calcFibonacciNum.test.js
+++ src/features/test/calcFibonacciNum.test.js
@@ -2,7 +2,28 @@
 /* eslint no-undef: "error" */
 import calcFibonacciNum from '../services/fibonacciNumService'
 
-// till 1474
+/*
+ ** The 53-bit significand precision gives from 15 to 17 significant decimal digits precision (2−53 ≈ 1.11 × 10−16)
+ **
+ ** Precision limitations on integer values
+ **
+ ** Integers from −2^53 to 2^53 (−9,007,199,254,740,992 to 9,007,199,254,740,992) can be exactly represented
+ ** Integers between 2^53 and 2^54 = 18,014,398,509,481,984 round to a multiple of 2 (even number)
+ ** Integers between 2^54 and 2^55 = 36,028,797,018,963,968 round to a multiple of 4
+ **
+ ** See Also: https://en.wikipedia.org/wiki/Double-precision_floating-point_format#JavaScript
+ **
+ ** Number Encoding
+ **
+ ** The largest value a number can hold is 2^1024 - 1 (with the exponent being 1023 and the mantissa being 0.1111… in base 2),
+ ** which is obtainable via Number.MAX_VALUE.
+ ** Values higher than that are replaced with the special number constant Infinity.
+ **
+ ** See Also: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+ **
+ ** queueNumber till 1474
+ */
+
 describe('test calcFibonacciNum function', () => {
     test('5th Fibonacci number is 5', () => {
         expect(calcFibonacciNum(5)).toBe(5)
