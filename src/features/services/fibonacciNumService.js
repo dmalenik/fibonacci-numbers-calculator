8948722e1db9726c178b49d30c58bcc32a9ac022
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-23 01:19:44 +0300

    feat(fibonacciNumService): rewrite the function

--- src/features/services/fibonacciNumService.js
+++ src/features/services/fibonacciNumService.js
@@ -1,12 +1,11 @@
 const calcFibonacciNum = (queueNumber) => {
     // Binet formula
     const sqrt5 = Math.sqrt(5)
-    const e1 = (1 + sqrt5) ** queueNumber
-    const e2 = (1 - sqrt5) ** queueNumber
-    const divider = 2 ** queueNumber * sqrt5
-    const fibonacciNumber = (e1 - e2) / divider
+    const e1 = ((1 + sqrt5) / 2) ** queueNumber
+    const e2 = ((1 - sqrt5) / 2) ** queueNumber
+    const fibonacciNumber = (e1 - e2) / sqrt5
 
-    return Math.floor(fibonacciNumber)
+    return Math.round(fibonacciNumber)
 }
 
 export default calcFibonacciNum
