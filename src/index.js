3fcc81c0a98faaf90f8b3eeb14042ed80a27a5d9
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-27 22:42:36 +0300

    fix(SEO): set summary description

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.scss'
 
 // Accessibility issues
 document.querySelector('html').setAttribute('lang', 'en-US')
+
+// SEO issues
+const summary = document.createElement('meta')
+
+summary.setAttribute('name', 'description')
+summary.setAttribute(
+    'content',
+    'Application: Fibonacci Numbers Calculator, Author: Dmitriy Malenik, Year: 2023, Purpose: By providing the sequence number in the input field you may find a Fibonacci number'
+)
+
+document.querySelector('head').appendChild(summary)
+
+// Application installation
 let app = App()
 document.body.appendChild(app)
 
